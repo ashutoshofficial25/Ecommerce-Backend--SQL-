f593487cab586e4539fc00766a425bb47bba1f40
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-11-17 09:40:27 +0530

    gg

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,8 +1,54 @@
 const productModel = require("../models/productModel");
 
-const getAllProducts = (req, res) => {};
+const getAllProducts = async (req, res) => {
+  try {
+    let products = await productModel.findAll();
+    res.status(201).json({
+      status: "success",
+      message: "All products",
+      total: products.length,
+      data: products,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Error",
+      message: "Something went wrong",
+      data: error,
+    });
+  }
+};
 
-const getAllProductById = (req, res) => {};
+const getAllProductById = async (req, res) => {
+  const productId = req.params.productId;
+  if (productId) {
+    let product = await productModel.findAll({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (product.length > 0) {
+      res.status(201).json({
+        status: "success",
+        message: "success",
+        data: product,
+      });
+    } else {
+      res.status(404).json({
+        status: "Error",
+        message: "Unavailable",
+        data: null,
+      });
+    }
+  } else {
+    res.status(404).json({
+      status: "Error",
+      message: "Invlid categoryId",
+      data: null,
+    });
+  }
+};
+
 const createProduct = async (req, res) => {
   const { name, price, categoryId } = req.body;
 
@@ -14,7 +60,26 @@ const createProduct = async (req, res) => {
       message: "Categody Added",
       data: product,
     });
-  } catch (error) {}
+  } catch (error) {
+    res.status(400).json({
+      status: "Error",
+      message: "Something went wrong",
+      data: error,
+    });
+  }
 };
 
+const updateProduct = async (req, res) => {
+  let productId = req.params.Id;
+  if (!productId) {
+    return res.status(404).json({
+      status: "Error",
+      message: "Id in not valid",
+      data: null,
+    });
+  }
+  try {
+    let updatedProduct = await productModel.update();
+  } catch (error) {}
+};
 module.exports = { getAllProducts, getAllProductById, createProduct };
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 router.get("/", productController.getAllProducts);
 
+router.get("/:productId", productController.getAllProductById);
+
 router.post("/", productController.createProduct);
 
 module.exports = router;
